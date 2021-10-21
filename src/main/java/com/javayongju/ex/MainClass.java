19de265ff96c236523bd821596f46c3232cb7be9
GoRyeoIn <89904365+znzty15@users.noreply.github.com>
2021-10-21 11:54:58 +0900

    Spring Container life cycle

---
+++ src/main/java/com/javayongju/ex/MainClass.java
@@ -0,0 +1,25 @@
+package com.javayongju.ex;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext(); //컨테이너 생성
+		
+		
+		ctx.load("classpath:applicationCTX.xml"); //컨테이너 설정
+		
+		ctx.refresh(); //컨테이너 설정
+		
+		
+		Student student = ctx.getBean("student", Student.class); //컨테이너 사용
+		System.out.println(student.getName()); //이름
+		System.out.println(student.getAge());//나이
+		
+		ctx.close(); //컨테이너 종료
+	}
+
+}
